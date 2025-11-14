5b8780e5c4ab469a0e9188e04e260d748e6e669c
sharansudheer <sharan.sharansudheer@gmail.com>
2025-11-14 13:48:18 +0530

    push:scaffolding
    scaffolding code

---
+++ pydasan_dsa_template/data_structures/stack/using_lists.py
@@ -0,0 +1,18 @@
+class stack_using_list:
+    def __init__(self):
+        self.counter=-1
+        self.stack_size = 0
+
+    def stack_init(self,size,counter):
+        the_stack = []
+        stack_size = size
+        return the_stack, counter
+    
+    def stack_push(self, item, counter, stack_size, the_stack[]):
+
+        if counter > stack_size:
+            print("Stack Overflow")
+        else:
+            the_stack.append(item)
+        return the_stack
+    
